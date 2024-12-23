0818b74c6fe6f7e8510385056a2bc1f544562d0c
jaredtrh <jaredtrh20@gmail.com>
2024-12-23 19:40:55 +0800

    Day23

---
+++ src/Day23.kt
@@ -0,0 +1,69 @@
+fun main() {
+    fun getAdj(input: List<String>): Map<String, List<String>> {
+        val adj: MutableMap<String, MutableList<String>> = mutableMapOf()
+        for (line in input) {
+            val (a, b) = line.split('-')
+            adj.getOrPut(a) { mutableListOf() }.add(b)
+            adj.getOrPut(b) { mutableListOf() }.add(a)
+        }
+        return adj
+    }
+
+    fun part1(input: List<String>): Int {
+        val adj = getAdj(input)
+        val split = input.map { it.split('-') }
+        val edge = (split.map { (a, b) -> Pair(a, b) } + split.map { (a, b) -> Pair(b, a) }).toSet()
+        val ans: MutableList<Triple<String, String, String>> = mutableListOf()
+        for ((com, nbs) in adj) {
+            for (i in nbs.indices) {
+                for (j in i + 1..< nbs.size) {
+                    if (
+                        edge.contains(Pair(com, nbs[i])) &&
+                        edge.contains(Pair(com, nbs[j])) &&
+                        edge.contains(Pair(nbs[i], nbs[j])) &&
+                        (com[0] == 't' || nbs[i][0] == 't' || nbs[j][0] == 't')
+                    ) {
+                        val (a, b, c) = arrayOf(com, nbs[i], nbs[j]).sorted()
+                        ans.add(Triple(a, b, c))
+                    }
+                }
+            }
+        }
+        return ans.distinct().size
+    }
+
+    fun part2(input: List<String>): String {
+        val adj = getAdj(input)
+        for ((com, nbs) in adj) {
+            val vis = nbs.toMutableSet()
+            vis.add(com)
+            val cnts: MutableList<Int> = mutableListOf()
+            for (nb in nbs) {
+                var cnt = 0
+                for (x in adj[nb]!!)
+                    if (vis.contains(x))
+                        cnt += 1
+                cnts.add(cnt)
+            }
+            val total = nbs.size + cnts.sum()
+            for (i in cnts.indices) {
+                if (total - cnts[i] * 2 == nbs.size * (nbs.size - 1)) {
+                    val ans = mutableListOf(com)
+                    ans.addAll(nbs.take(i))
+                    ans.addAll(nbs.drop(i + 1))
+                    ans.sort()
+                    return ans.joinToString(",")
+                }
+            }
+        }
+        return "" // unreachable
+    }
+
+    val testInput = readInput("Day23_test")
+    check(part1(testInput) == 7)
+    check(part2(testInput) == "co,de,ka,ta")
+
+    val input = readInput("Day23")
+    part1(input).println()
+    part2(input).println()
+}
